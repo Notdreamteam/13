9f48fcb613d3f0ffc23459f4d80272f4c350d4b2
Notdreamteam <dtisckov@gmail.com>
2025-02-28 08:56:55 +0300

    Add files via upload

---
+++ z8/z8/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z8
+{
+    class Program
+    {
+        // Класс для работы с восьмеричными числами
+        public class OctalConverter
+        {
+            // Метод для перевода восьмеричного числа в десятичное
+            public int ConvertOctalToDecimal(int octalNumber)
+            {
+                int decimalValue = 0;
+                int baseValue = 1;
+
+                while (octalNumber > 0)
+                {
+                    int lastDigit = octalNumber % 10;
+                    if (lastDigit >= 8)
+                    {
+                        throw new ArgumentException("Некорректное восьмеричное число.");
+                    }
+
+                    decimalValue += lastDigit * baseValue;
+                    octalNumber /= 10;
+                    baseValue *= 8;
+                }
+
+                return decimalValue;
+            }
+        }
+
+        // Класс для работы с массивами
+        public class ArrayProcessor
+        {
+            private OctalConverter _octalConverter;
+
+            public ArrayProcessor()
+            {
+                _octalConverter = new OctalConverter();
+            }
+
+            // Метод для перевода массива восьмеричных чисел в десятичные
+            public int[] ConvertOctalArrayToDecimal(int[] octalArray)
+            {
+                int[] decimalArray = new int[octalArray.Length];
+                for (int i = 0; i < octalArray.Length; i++)
+                {
+                    decimalArray[i] = _octalConverter.ConvertOctalToDecimal(octalArray[i]);
+                }
+                return decimalArray;
+            }
+        }
+
+        // Основной класс программы
+        public class MainClass
+        {
+            public static void Main(string[] args)
+            {
+                // Создаем экземпляр класса ArrayProcessor
+                ArrayProcessor arrayProcessor = new ArrayProcessor();
+
+                // Ввод массива из 9 двузначных восьмеричных чисел
+                int[] octalArray = new int[9];
+                Console.WriteLine("Введите 9 двузначных чисел в восьмеричной системе счисления:");
+
+                for (int i = 0; i < 9; i++)
+                {
+                    Console.Write($"Элемент {i + 1}: ");
+                    octalArray[i] = int.Parse(Console.ReadLine());
+                }
+
+                try
+                {
+                    // Перевод массива восьмеричных чисел в десятичные
+                    int[] decimalArray = arrayProcessor.ConvertOctalArrayToDecimal(octalArray);
+
+                    // Вывод нового массива
+                    Console.WriteLine("Новый массив (в десятичной системе счисления):");
+                    for (int i = 0; i < decimalArray.Length; i++)
+                    {
+                        Console.Write($"{decimalArray[i]} ");
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    // Обрабатываем ошибку, если введено некорректное восьмеричное число
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ z9/z9/Class1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z9
+{
+    class Class1
+    {
+        // Класс для работы с числами
+        public class NumberProcessor
+        {
+            // Метод для извлечения старшего разряда (десятков) из двузначного числа
+            public int ExtractTensDigit(int number)
+            {
+                if (number < 10 || number > 99)
+                {
+                    throw new ArgumentException("Число должно быть двузначным.");
+                }
+
+                return number / 10; // Возвращаем десятки
+            }
+        }
+
+        // Класс для работы с массивами
+        public class ArrayProcessor
+        {
+            private NumberProcessor _numberProcessor;
+
+            public ArrayProcessor()
+            {
+                _numberProcessor = new NumberProcessor();
+            }
+
+            // Метод для получения массива из старших разрядов элементов исходного массива
+            public int[] ExtractTensDigitsFromArray(int[] originalArray)
+            {
+                int[] newArray = new int[originalArray.Length];
+                for (int i = 0; i < originalArray.Length; i++)
+                {
+                    newArray[i] = _numberProcessor.ExtractTensDigit(originalArray[i]);
+                }
+                return newArray;
+            }
+        }
+
+        // Основной класс программы
+        public class MainClass
+        {
+            public static void Main(string[] args)
+            {
+                // Создаем экземпляр класса ArrayProcessor
+                ArrayProcessor arrayProcessor = new ArrayProcessor();
+
+                // Ввод массива из 7 двузначных целых чисел
+                int[] originalArray = new int[7];
+                Console.WriteLine("Введите 7 двузначных целых чисел:");
+
+                for (int i = 0; i < 7; i++)
+                {
+                    Console.Write($"Элемент {i + 1}: ");
+                    originalArray[i] = int.Parse(Console.ReadLine());
+                }
+
+                try
+                {
+                    // Получение нового массива из старших разрядов
+                    int[] newArray = arrayProcessor.ExtractTensDigitsFromArray(originalArray);
+
+                    // Вывод нового массива
+                    Console.WriteLine("Новый массив (старшие разряды):");
+                    for (int i = 0; i < newArray.Length; i++)
+                    {
+                        Console.Write($"{newArray[i]} ");
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    // Обрабатываем ошибку, если введено некорректное число
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
